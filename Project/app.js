11d1ab7cbb3b6ad8137f764438d0e6daa08f0d21
Hazmat11 <ebertrand@gaming.tech>
2022-03-29 17:38:29 +0200

    collision with object

--- Project/app.js
+++ Project/app.js
@@ -13,19 +13,40 @@ const config = {
   }
 }
 
-var game = new Phaser.Game(config, 'gameContainer')
+var game = new Phaser.Game(config);
 let test
 let cursors
+var platform;
 
 
 function preload() {
   this.load.image('test', 'img/test.png')
+  this.load.image('platform', 'img/platform.png')
 }
 
 function create() {
+
+  platform = this.physics.add.image(500, 100, 'platform')
+  platform.setImmovable(true)
+  platform.body.allowGravity = false
+
   test = this.physics.add.image(400, 250, 'test')
   test.body.collideWorldBounds = true
   cursors = this.input.keyboard.createCursorKeys()
+
+  /*this.physics.add.collider(
+    test,
+    platform,
+    function (_test, _platform)
+    {
+      if (_test.body.touching.up && _platform.body.touching.down)
+      {
+        create
+      }
+    }
+  )*/
+
+  this.physics.add.collider(test, platform);
 }
 
 function update() {
